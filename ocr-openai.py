8f6bf5fb73e275f9adee0613fa8b3fd4aa2dd025
Yewon Choi <ywchoi0625@gmail.com>
2024-02-03 07:23:40 +0900

    주석 정리

--- ocr-openai.py
+++ ocr-openai.py
@@ -28,7 +28,8 @@ for i in range(N):
     link = list(my_dict.keys())[i]
     ocr = list(my_dict.values())[i]
 
-    prompt = ocr + "\n 이 텍스트에서 지원자격, 우대사항 을 뽑아서 '지원자격:' , '우대사항:' 이렇게 적어줘"
+    # 원하는 값들을 추출하기 위한 프롬프트
+    prompt = ocr + "\n 이 텍스트에서 지원자격, 우대사항 을 뽑아서 '지원자격:' , '우대사항:' 이런 형태로 작성해줘. 찾지 못했을 경우 빈 문자열 ""을 리턴해줘."
     
     chat_completion = client.chat.completions.create(
         messages=[
@@ -41,16 +42,17 @@ for i in range(N):
     )
 
     res = chat_completion.choices[0].message.content
-    # "지원자격:"과 "우대사항:" 사이의 값을 가져오기
+    # "지원자격:"과 "우대사항:" 사이의 값을 가져옴
     requirement_start_index = res.find("지원자격:") + len("지원자격:")
     requirement_end_index = res.find("우대사항:")
     requirement = res[requirement_start_index:requirement_end_index].strip().replace("\'", "")
 
-    # "우대사항:" 이후의 값을 가져오기
+    # "우대사항:" 이후의 값을 가져옴
     preferred_start_index = res.find("우대사항:") + len("우대사항:")
     preferred = res[preferred_start_index:].strip().replace("\'", "")
 
     print(i)
+    # 저장되어있던 값 업데이트
     sql = f"update event set requirement='{requirement}', preferred='{preferred}' where image like '{link}'"
     cur.execute(sql)
     conn.commit()
